17609bc1a73a6388d5acec605207af2c571ebef3
PolaSanthoshi <polasanthoshi39@gmail.com>
2023-08-14 16:57:46 +0530

    added_netlify_function

--- src/login.tsx
+++ src/login.tsx
@@ -43,9 +43,10 @@ export default function Login() {
     //         alert('Invalid id')
     //     }
     // })
-   fetch('https://64da0cb4555a762edf59c0e1--transcendent-toffee-89a6b6.netlify.app/.netlify/functions/login')
-    .then(response=>response.json())
-    .then(val=>val.data)
+  axios.get('/.netlify/functions/login')
+    .then(response=>console.log(response.data))
+    .catch(k=>console.log('error'))
+   
   };
   const isLoginDisabled =
    (id === '' || id.length !== 4)
