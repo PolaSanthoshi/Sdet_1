f10992c1b2597dd34bd23dab2a5e01709e086df8
PolaSanthoshi <polasanthoshi39@gmail.com>
2023-08-14 16:31:41 +0530

    added_netlify_function

--- pages/functions/login.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async (event, context) => {
-    console.log(event,'aaaaaaaaaaaaaa')
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: 'Hello from Netlify Function!' }),
-    };
-  };
-  
-
