9d4a47f84adbe94c188a12266254cafa19855dc4
PolaSanthoshi <polasanthoshi39@gmail.com>
2023-08-15 14:35:54 +0530

    added_netlify_function

--- src/login.tsx
+++ src/login.tsx
@@ -42,7 +42,7 @@ export default function Login() {
     //         alert('Invalid id')
     //     }
     // })
-  axios.get('/functions/loginApi')
+  axios.get('.netlify/functions/loginApi')
     .then(response=>console.log(response.data))
     .catch(k=>console.log('error'))
    
