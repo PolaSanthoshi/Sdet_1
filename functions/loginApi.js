32defad03779f1247f9cece2b7617806a239a125
PolaSanthoshi <polasanthoshi39@gmail.com>
2023-08-16 15:43:47 +0530

    added_netlify_function

--- functions/loginApi.js
+++ functions/loginApi.js
@@ -2,7 +2,7 @@ import { setCookie } from "nookies";
 exports.handler=async(event,context)=>{
 const users=[{id:1000,role:'employee'},{id:1001,role:'employee'},
 	{id:1002,role:'employee'},{id:1003,role:'employee'},{id:1004,role:'admin'},{id:1005,role:'admin'}]
-const {id,role}=event.body;
+const {id,role}=JSON.parse(event.body)
 const {rawUrl}=event;
 const validUser=users.findIndex((e)=>id==e.id && e.role==role)
 if(validUser>=0){
@@ -25,7 +25,7 @@ if(validUser>=0){
 }else{
         return {
 			statusCode:200,
-			body:JSON.stringify({valid:false,isLoggedIn:false,validUser:validUser,body:event.body})
+			body:JSON.stringify({valid:false,isLoggedIn:false,validUser:validUser,body:event.body,id:id,role:role})
 		}
 }
 	
