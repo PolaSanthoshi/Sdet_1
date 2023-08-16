f499d453e4c8205b38a51c5dfe3cda8b4a0e1532
PolaSanthoshi <polasanthoshi39@gmail.com>
2023-08-16 10:58:34 +0530

    added_netlify_function

---
+++ pages/api/functions/loginApi.js
@@ -0,0 +1,15 @@
+exports.handler = async (event, context) => {
+	console.log("api was called")
+	try { // Perform your API logic here
+	  const user = { id: 1, name: 'John' };
+	  return {
+		statusCode: 200,
+		body: JSON.stringify(user),
+	  };
+	} catch (error) {
+	  return {
+		statusCode: 500,
+		body: JSON.stringify({ message: 'Internal server error' }),
+	  };
+	}
+  }
