5717bc8cc1fbd19d1228b3e53c550fbd22e4fdcd
PolaSanthoshi <polasanthoshi39@gmail.com>
2023-08-16 13:30:22 +0530

    added_netlify_function

--- functions/loginApi.js
+++ functions/loginApi.js
@@ -4,6 +4,7 @@ const users=[{id:1000,role:'employee'},{id:1001,role:'employee'},
 	{id:1002,role:'employee'},{id:1003,role:'employee'},{id:1004,role:'admin'},{id:1005,role:'admin'}]
 const {id,role}=event.body;
 const {rawUrl}=event;
+console.log(id,role)
 const validUser=users.find((each)=>id==each.id&&each.role===role)
 if(validUser){
         setCookie({rawUrl},'isLoggedIn',true,{
@@ -25,7 +26,7 @@ if(validUser){
 }else{
         return {
 			statusCode:200,
-			body:JSON.stringify({valid:false,isLoggedIn:false,validUser})
+			body:JSON.stringify({valid:false,isLoggedIn:false})
 		}
 }
 	
