d51f836143ca995ecda26c33dba2035bcdebf0fe
PolaSanthoshi <polasanthoshi39@gmail.com>
2023-08-16 13:14:22 +0530

    added_netlify_function

--- functions/loginApi.js
+++ functions/loginApi.js
@@ -1,15 +1,32 @@
-exports.handler = async (event, context) => {
-	console.log("api was called")
-	try { // Perform your API logic here
-	  const user = { id: 1, name: 'John' };
-	  return {
-		statusCode: 200,
-		body: JSON.stringify({body:event.body,url:event.url,event}),
-	  };
-	} catch (error) {
-	  return {
-		statusCode: 500,
-		body: JSON.stringify({ message: 'Internal server error' }),
-	  };
-	}
-  }
+import { setCookie } from "nookies";
+exports.handler=async(event,context)=>{
+const users=[{id:1000,role:'employee'},{id:1001,role:'employee'},
+	{id:1002,role:'employee'},{id:1003,role:'employee'},{id:1004,role:'admin'},{id:1005,role:'admin'}]
+const {id,role}=event.body;
+const {rawUrl}=event;
+const validUser=users.find((each)=>id==each.id&&each.role===role)
+if(validUser){
+        setCookie({rawUrl},'isLoggedIn',true,{
+        path:'/',
+        httpOnly:true
+})
+         setCookie({rawUrl},'id',id,{
+         path:'/',
+         httpOnly:true
+})         
+        setCookie({rawUrl},'role',role,{
+    	httpOnly:true,
+     	path:'/'
+})
+        return {
+			statusCode:200,
+			body:JSON.stringify({valid:true,isLoggedIn:true})
+		}
+}else{
+        return {
+			statusCode:200,
+			body:JSON.stringify({valid:false,isLoggedIn:false})
+		}
+}
+	
+}
