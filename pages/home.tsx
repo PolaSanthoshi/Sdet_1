cd894555c8c2ca479e7a4dc02d1b4ced3c3505c0
PolaSanthoshi <polasanthoshi39@gmail.com>
2023-08-16 17:47:44 +0530

    added_netlify_function

--- pages/home.tsx
+++ pages/home.tsx
@@ -14,6 +14,7 @@ export async function getServerSideProps(context:any){
      // const {id,role}=context.query;
      const {parseCookies}=require('nookies')
      const {isLoggedIn,id,role}=parseCookies(context);
+     console.log(isLoggedIn,id,role)
      const data=await fetch('https://netlify-code--transcendent-toffee-89a6b6.netlify.app/.netlify/functions/menu');
      const menuData= await data.json();
      const response=await fetch(`https://netlify-code--transcendent-toffee-89a6b6.netlify.app/.netlify/functions/employee`);
