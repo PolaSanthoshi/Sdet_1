0df15d448d5f4a0043b8d76d1d5bdeacc192d816
PolaSanthoshi <polasanthoshi39@gmail.com>
2023-08-16 15:19:07 +0530

    added_netlify_function

--- functions/loginApi.js
+++ functions/loginApi.js
@@ -1,32 +1,41 @@
 import { setCookie } from "nookies";
-exports.handler=async(event,context)=>{
-const users=[{id:1000,role:'employee'},{id:1001,role:'employee'},
-	{id:1002,role:'employee'},{id:1003,role:'employee'},{id:1004,role:'admin'},{id:1005,role:'admin'}]
-const {id,role}=event.body;
-const {rawUrl}=event;
-const validUser=users.findIndex((e)=>id==e.id && e.role==role)
-if(validUser>=0){
-        setCookie({rawUrl},'isLoggedIn',true,{
-        path:'/',
-        httpOnly:true
-})
-         setCookie({rawUrl},'id',id,{
-         path:'/',
-         httpOnly:true
-})         
-        setCookie({rawUrl},'role',role,{
-    	httpOnly:true,
-     	path:'/'
-})
-        return {
-			statusCode:200,
-			body:JSON.stringify({valid:true,isLoggedIn:true,validUser:validUser})
-		}
-}else{
-        return {
-			statusCode:200,
-			body:JSON.stringify({valid:false,isLoggedIn:false,validUser:validUser,id:id,role:role})
-		}
-}
-	
-}
+exports.handler = async (event, context) => {
+  const users = [
+    { id: 1000, role: 'employee' },
+    { id: 1001, role: 'employee' },
+    { id: 1002, role: 'employee' },
+    { id: 1003, role: 'employee' },
+    { id: 1004, role: 'admin' },
+    { id: 1005, role: 'admin' }
+  ];
+  const { id, role } = JSON.parse(event.body); // Parse the JSON body
+  const validUser = users.findIndex((e) => id == e.id && e.role == role);
+  if (validUser >= 0) {
+    // Construct the cookie path based on the function's route
+    const cookiePath = `/`;
+    setCookie(null, 'isLoggedIn', 'true', {
+      path: cookiePath,
+      httpOnly: true
+    });
+    setCookie(null, 'id', id, {
+      path: cookiePath,
+      httpOnly: true
+    });
+    setCookie(null, 'role', role, {
+      path: cookiePath,
+      httpOnly: true
+    });
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ valid: true, isLoggedIn: true, validUser: validUser })
+    };
+  } else {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ valid: false, isLoggedIn: false, validUser: validUser, id })
+    };
+  }
+};
+
+
+
