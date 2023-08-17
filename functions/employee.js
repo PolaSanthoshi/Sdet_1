d6b1b7b89b8a30cdbd04376b78e2f94e6b0c0f3a
PolaSanthoshi <polasanthoshi39@gmail.com>
2023-08-17 14:59:15 +0530

    created_netlify_function

--- functions/employee.js
+++ functions/employee.js
@@ -1,40 +1,41 @@
 
-const userData=[{id:1000,haveLunch:"yes"}];
+const userData=[{id:1000,haveLunch:"yes"},{id:1001,haveLunch:"yes"}];
 exports.handler=async(event,context)=>{
    
-    const {val,id}=JSON.parse(event.body)
-        // switch(event.httpMethod){
-        //     case 'POST':{
-        //        !userData.id?userData.id=id:''
-        //         userData.haveLunch=val;
-                // const x=userData.findIndex((element)=>element.id==id);
-                // if(x==-1){
-                //     userData.push({id,haveLunch:val}) 
-                // }else{
-                //     userData[x].haveLunch=val
-                // }
+         const id=event.queryStringParameters.id;
+        switch(event.httpMethod){
+            case 'POST':{
+            //    !userData.id?userData.id=id:''
+            //     userData.haveLunch=val;
+                const {val}=JSON.parse(event.body)
+                const x=userData.findIndex((element)=>element.id==id);
+                if(x==-1){
+                    userData.push({id,haveLunch:val}) 
+                }else{
+                    userData[x].haveLunch=val
+                }
                
                return{
                   statuscode:200,
                   body:JSON.stringify({userData,x:event.queryStringParameters})
                }
         }
-        //    case 'GET':    
-        //         const data=userData.find((element)=>element.id==id)
-        //         // console.log(data,'employeeDataOfAPi',userData)
-        //         // console.log(userData.find((element)=>element.id==id),'find')
-        //         // data?res.status(200).json(data):res.status(200).json('not found')
-        //         if(data){
-        //             return{
-        //                 statuscode:200,
-        //                 body:JSON.stringify({data})
+           case 'GET':    
+                const data=userData.find((element)=>element.id==id)
+                // console.log(data,'employeeDataOfAPi',userData)
+                // console.log(userData.find((element)=>element.id==id),'find')
+                // data?res.status(200).json(data):res.status(200).json('not found')
+                if(data){
+                    return{
+                        statuscode:200,
+                        body:JSON.stringify({data})
 
-        //             }
-        //         }else{
-        //             return{
-        //                 statuscode:200,
-        //                 body:JSON.stringify('not found')
-        //             }
-        //         }    
-//     }
-// }
+                    }
+                }else{
+                    return{
+                        statuscode:200,
+                        body:JSON.stringify('not found')
+                    }
+                }    
+    }
+}
