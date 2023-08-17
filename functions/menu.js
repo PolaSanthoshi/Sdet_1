af981b0397e0f10525249b9bada6a894d235ac09
PolaSanthoshi <polasanthoshi39@gmail.com>
2023-08-17 12:25:39 +0530

    added_netlify_function

--- functions/menu.js
+++ functions/menu.js
@@ -1,20 +1,20 @@
 const menuData=['rice','dal','salad'];
 exports.handler=async(event,context)=>{
     
-    // switch (event.httpMethod){
-    //     case 'POST':
-    //         menuData.length=0
-    //         const {menu}=JSON.parse(event.body)
-    //         menu.forEach((element)=>menuData.push(element))
-    //         return{
-    //             statusCode:200,
-    //             body:JSON.stringify({menu})
-    //         }
-    //         break
-    //     default:
+    switch (event.httpMethod){
+        case 'POST':
+            menuData.length=0
+            const {menu}=JSON.parse(event.body)
+            menu.forEach((element)=>menuData.push(element))
+            return{
+                statusCode:200,
+                body:JSON.stringify({menu})
+            }
+            break
+        default:
             return{
                 statusCode:200,
                 body:JSON.stringify({menuData})
             }
     }
-// }
+}
