d6e9ba327dfe5f4d9b91cf3c5ed015020d922a48
PolaSanthoshi <polasanthoshi39@gmail.com>
2023-08-17 17:57:30 +0530

    added_netlify_function

--- functions/loginApi.js
+++ functions/loginApi.js
@@ -1,5 +1,5 @@
 import { setCookie } from "nookies";
-const cookie=require('cookie')
+import {cookie} from 'cookie';
 exports.handler=async(event,context)=>{
 const users=[{id:1000,role:'employee'},{id:1001,role:'employee'},
 	{id:1002,role:'employee'},{id:1003,role:'employee'},{id:1004,role:'admin'},{id:1005,role:'admin'}]
