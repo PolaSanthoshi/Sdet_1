6033d128fab13a4638b377bfa50614909ebe708a
PolaSanthoshi <polasanthoshi39@gmail.com>
2023-08-17 14:29:46 +0530

    created_netlify_function

--- functions/loginApi.js
+++ functions/loginApi.js
@@ -3,7 +3,6 @@ exports.handler=async(event,context)=>{
 const users=[{id:1000,role:'employee'},{id:1001,role:'employee'},
 	{id:1002,role:'employee'},{id:1003,role:'employee'},{id:1004,role:'admin'},{id:1005,role:'admin'}]
 const {id,role}=JSON.parse(event.body)
-const {rawUrl}=event;
 const validUser=users.findIndex((e)=>id==e.id && e.role==role)
 if(validUser>=0){
         setCookie({res:event},'isLoggedIn',true,{
