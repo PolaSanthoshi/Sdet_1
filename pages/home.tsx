3d776e1f37bfca0f48f8fe71cdf1999ee3ee56e8
PolaSanthoshi <polasanthoshi39@gmail.com>
2023-08-18 10:41:51 +0530

    added_netlify_function

--- pages/home.tsx
+++ pages/home.tsx
@@ -4,7 +4,7 @@ import Home from "@/src/home";
 import { parseCookies } from "nookies";
 export default function Homee(props:{menuData:string[],userData:{id:string,haveLunch:string},id:string,role:string,isLoggedIn:any}){
      const router=useRouter();
-     console.log(props.isLoggedIn , typeof props.isLoggedIn)
+     console.log(props.isLoggedIn)
      // const {id}=router.query;
   return  <div>
      <Home menuData={props.menuData} userData={props.userData} id={props.id} role={props.role}/>
@@ -20,16 +20,16 @@ export async function getServerSideProps(context:any){
      const menuData= await data.json();
      const response=await fetch(`https://netlify-code--transcendent-toffee-89a6b6.netlify.app/.netlify/functions/employee?id=${id}`);
      const userData=await response.json();
-     if(1){
+     if(isLoggedIn==='true'){
           return {
                props:{menuData,userData,id,role,isLoggedIn}
               }
      }
-//            return {
-//             redirect:{
-//                 destination:'/login',
-//                 permanent:false,
-//      }
-// }
+           return {
+            redirect:{
+                destination:'/login',
+                permanent:false,
+     }
+}
 
 }
