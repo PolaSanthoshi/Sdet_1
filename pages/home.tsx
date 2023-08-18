ffd46013313365a1c7333fbf129effbba168b32c
PolaSanthoshi <polasanthoshi39@gmail.com>
2023-08-18 15:55:03 +0530

    over-write

--- pages/home.tsx
+++ pages/home.tsx
@@ -16,9 +16,9 @@ export async function getServerSideProps(context:any){
      const {parseCookies}=require('nookies')
      const {isLoggedIn,id,role}=parseCookies(context);
      console.log(isLoggedIn,id,role)
-     const data=await fetch('https://netlify-code--transcendent-toffee-89a6b6.netlify.app/.netlify/functions/menu');
+     const data=await fetch('http://localhost:3000/api/menu');
      const menuData= await data.json();
-     const response=await fetch(`https://netlify-code--transcendent-toffee-89a6b6.netlify.app/.netlify/functions/employee?id=${id}`);
+     const response=await fetch(`http://localhost:3000/api/employee/${id}`);
      const userData=await response.json();
      if(1){
           return {
