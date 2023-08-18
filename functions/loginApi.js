2c1813ef90aa4f1ef008637c09ef654e921ed1b1
PolaSanthoshi <polasanthoshi39@gmail.com>
2023-08-18 12:27:52 +0530

    added_netlify_function

--- functions/loginApi.js
+++ functions/loginApi.js
@@ -9,16 +9,15 @@ cookie_options={
         secure:true,
         path:'/'
 }
-const logInCookie = serialize('isLoggedIn', 'true', cookie_options)
+const logInCookie = serialize('isLoggedIn', true, cookie_options)
 const idCookie = serialize('id', id, cookie_options);
 const roleCookie = serialize('role', role, cookie_options);
-const combinedCookies = [idCookie, logInCookie,roleCookie].join('; ');
 if(validUser>=0){
 
         return {
 			statusCode:200,
                         headers:{
-                                'Set-Cookie':combinedCookies
+                                'Set-Cookie':[logInCookie,idCookie,roleCookie]
                         },
 			body:JSON.stringify({valid:true,isLoggedIn:true,validUser:validUser})
 		}
