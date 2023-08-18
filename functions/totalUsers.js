f01499f7ec1116a76547b562757934793fe19a65
PolaSanthoshi <polasanthoshi39@gmail.com>
2023-08-18 13:18:15 +0530

    added_netlify_function

--- functions/totalUsers.js
+++ functions/totalUsers.js
@@ -1,5 +1,6 @@
-import { userData } from "./employee"
-exports.handler=async()=>{
+// import { userData } from "./employee"
+const userData=[{id:1000,haveLunch:"yes"},{id:1001,haveLunch:"yes"}];
+exports.handler=async(event,context)=>{
     const filteredData=userData.filter((item)=>item.haveLunch==='yes')
     return {
         statusCode:200,
