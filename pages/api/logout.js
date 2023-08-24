f8e233c0a91aab99031559432ac0c15f60614b7e
PolaSanthoshi <polasanthoshi39@gmail.com>
2023-08-24 15:20:01 +0530

    session_lock

--- pages/api/logout.js
+++ pages/api/logout.js
@@ -14,17 +14,10 @@ export default function handler(req,res){
     //     'id=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT'
     //   ]);
       
-    destroyCookie({res},'isLoggedIn',{
+    destroyCookie({res},'token',{
        path:'/',
        maxAge:-1
     })
-    destroyCookie({res},'id',{
-        path:'/',
-        maxAge:-1
-    })
-    destroyCookie({res},'role',{
-        path:'/',
-        maxAge:-1
-    })
+   
     return res.status(200).json('You have been Logged Out')
 }
