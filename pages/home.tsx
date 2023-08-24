4110211f6b18327b4642aa550818ac99f9e70801
PolaSanthoshi <polasanthoshi39@gmail.com>
2023-08-24 11:21:54 +0530

    session_lock

--- pages/home.tsx
+++ pages/home.tsx
@@ -20,7 +20,7 @@ export async function getServerSideProps(context:any){
      const menuData= await data.json();
      const response=await fetch(`https://netlify-code--transcendent-toffee-89a6b6.netlify.app/.netlify/functions/employee?id=${id}`);
      const userData=await response.json();
-     if(localStorage.getItem('token')){
+     if(1){
           return {
                props:{menuData,userData,isLoggedIn:isLoggedIn}
               }
