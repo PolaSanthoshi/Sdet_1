dae6cc2e6e76384cdb313d2dd7d1c88d1db92edf
PolaSanthoshi <polasanthoshi39@gmail.com>
2023-08-24 13:28:58 +0530

    session_lock

--- functions/auth.js
+++ functions/auth.js
@@ -1,4 +1,5 @@
 const jwt=require('jsonwebtoken')
+const {serialize}=require('cookie')
 exports.handler=async(event,context)=>{
     const secretKey='aby_kLXIOPKANJD'
     switch (event.httpMethod){
@@ -6,15 +7,15 @@ exports.handler=async(event,context)=>{
     const {id,role}=JSON.parse(event.body)
     const userVal={id,role,isLoggedIn:true}
     const token=jwt.sign(userVal,secretKey,{expiresIn:'1h'})
-//     const logInCookie = serialize('token', token, {
-//         secure:true,
-//         path:'/'
-// })
+    const logInCookie = serialize('token', token, {
+        secure:true,
+        path:'/'
+})
     return {
         statusCode:200,
         headers:{
             'Authorization':`Bearer ${token}`,
-           
+             'Set-Cookie':logInCookie
         },
         body:JSON.stringify('Created token')
     }
