e8915c7276dfbd2a2078f5946827832065787b37
PolaSanthoshi <polasanthoshi39@gmail.com>
2023-08-25 11:36:59 +0530

    Login_session_lock

--- pages/login.tsx
+++ pages/login.tsx
@@ -35,31 +35,33 @@ export default function login(){
     <Login/>
   </div>
 }
-// export function getServerSideProps(context:any){
-  // const jwt=require('jsonwebtoken')
-  // const {parseCookies}=require('nookies')
-  // const {token}=parseCookies(context);
-  //     const decodedPayLoad=jwt.decode(token);
-  //     const {isLoggedIn,id,role}=decodedPayLoad;
-  //     if(isLoggedIn && role=='employee'){
-  //       return{
-  //            redirect:{
-  //           destination:`/home`,
-  //           permanent:false
-  //       }
-  //   } }
-  //   if(isLoggedIn && role=='admin'){
-  //     return{
-  //          redirect:{
-  //         destination:`/admin`,
-  //         permanent:false
-  //     }
-  // }
+export function getServerSideProps(context:any){
+  const jwt=require('jsonwebtoken')
+  const {parseCookies}=require('nookies')
+  const {token}=parseCookies(context);
+  if(token){
+      const decodedPayLoad=jwt.decode(token);
+      const {isLoggedIn,id,role}=decodedPayLoad;
+      if(isLoggedIn && role=='employee'){
+        return{
+             redirect:{
+            destination:`/home`,
+            permanent:false
+        }
+    } }
+    if(isLoggedIn && role=='admin'){
+      return{
+           redirect:{
+          destination:`/admin`,
+          permanent:false
+      }
+  }
  
-  //    }
-//           return {
-//               props:{value:'Invalid ID'}
-//           }
+     }
+    }
+          return {
+              props:{value:'Invalid ID'}
+          }
    
 
-// }
+}
