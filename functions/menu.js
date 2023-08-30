59da9df1eb19a0cb744e478c441bfff9452ad82b
PolaSanthoshi <polasanthoshi39@gmail.com>
2023-08-30 16:46:03 +0530

    menu_api

--- functions/menu.js
+++ functions/menu.js
@@ -1,20 +1,42 @@
-const menuData=['rice','dal','salad'];
+import { createClient } from "@supabase/supabase-js";
 exports.handler=async(event,context)=>{
-    
+    const supabaseUrl = process.env.SUPABASE_URL;
+    const supabaseApiKey = process.env.SUPABASE_KEY;
+    const supabase = createClient(supabaseUrl, supabaseApiKey);
     switch (event.httpMethod){
         case 'POST':
-            menuData.length=0
-            const menu=JSON.parse(event.body)
-            menu.forEach((element)=>menuData.push(element))
+         const menu=event.body
+         const {data,error}=await supabase
+         .from('admin')
+         .update({menu:menu})
+         .eq('id',1031)
+         if(data){
             return{
                 statusCode:200,
-                body:JSON.stringify(menu)
+                body:JSON.stringify(data)
             }
-            
+         }else{
+            return{
+                statusCode:500,
+                body:JSON.stringify(error)
+            }
+         }
+
         case 'GET':
+            const  {data1,error1}=await supabase
+            .from('admin')
+            .select('menu')
+            .eq('id',1031)
+            if(error1){
+            return{
+                statusCode:500,
+                body:JSON.stringify(error1)
+            }
+        }else{
             return{
                 statusCode:200,
-                body:JSON.stringify(menuData)
+                body:JSON.stringify(data1)
             }
+        }
     }
 }
