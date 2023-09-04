46c50bdf732fd674fd636cd99b932c0d08d51997
PolaSanthoshi <polasanthoshi39@gmail.com>
2023-09-04 14:25:24 +0530

    clear_previous_data

--- functions/totalCount.js
+++ functions/totalCount.js
@@ -3,10 +3,13 @@ exports.handler=async(event,context)=>{
     const supabaseUrl = process.env.SUPABASE_URL;
     const supabaseApiKey = process.env.SUPABASE_KEY;
     const supabase = createClient(supabaseUrl, supabaseApiKey);
+    const date=new Date();
+    const formattedDate=date.toISOString.split('T')[0];
     const {data,error}=await supabase
     .from('confirmation')
     .select('response')
     .eq('response','yes')
+    .eq('date',formattedDate)
     const count=data.length;
     return{
         statusCode:200,
