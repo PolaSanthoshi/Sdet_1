3a923bfcd9f21f06ae1b1d411611c1de45436e59
PolaSanthoshi <polasanthoshi39@gmail.com>
2023-09-07 18:04:55 +0530

    added_mobile_view

--- src/home.tsx
+++ src/home.tsx
@@ -58,13 +58,13 @@ export default function Home(props: { menuData: string[],haveLunch:string,id:str
   }
   return (
     <div>
-    <div className={`bg-[#55647471]  ${showPopUp?'opacity-60 transition-opacity pointer-events-none':''}`}>
-      <nav className="pr-4 h-[70px] flex justify-between items-center relative ">
+    <div className={`bg-[#55647471] ${showPopUp?'opacity-60 transition-opacity pointer-events-none':''}`}>
+      <nav className="pr-4 md:h-[70px] h-[50px] flex justify-between items-center relative bg-black md:bg-transparent  ">
         <div className="flex items-center">
-          <div className="bg-black text-white min-w-[130px] p-2 rounded-r-lg">
+          <div className="bg-black  text-white min-w-[130px] p-2 rounded-r-lg">
             Welcome, <span className="font-bold">{name}</span>
           </div>
-          <Link href={"https://sdettech.com/"}  target="_blank">          
+          <Link href={"https://sdettech.com/"}  target="_blank" className="md:block hidden">          
           <div className=" p-2 text-lg font-semibold   rounded-md ml-5">S D E T</div>
           </Link>
           
@@ -76,37 +76,35 @@ export default function Home(props: { menuData: string[],haveLunch:string,id:str
         </div>
        
         <button
-          className="w-8 h-8 bg-black flex justify-center items-center text-white rounded-full hover:bg-neutral-400 focus:ring focus:ring-blue-200"
+          className="w-8 h-8 bg-black  justify-center items-center text-white rounded-full hover:bg-neutral-400 focus:ring focus:ring-blue-200 md:flex hidden"
           onClick={onUserIconClick}
         >
           <FaUser />
         </button>
+        <button className="bg-white p-1 rounded-md  font-semibold md:hidden block" onClick={handleLogout}>Logout</button>
         {isProfileVisible&&<div className="absolute cursor-pointer   right-4 -bottom-8  " onClick={handleLogout}>  
          <div className="w-full text-right flex justify-end"> <FaCaretUp/></div>
-         <div className="p-2 rounded-md text-white bg-black -mt-[6px] ">Logout</div>
+         <div className="p-2 rounded-md text-white bg-black -mt-[6px]  ">Logout</div>
         </div>}
       </nav>
-      <div className="w-full homeHeight flex">
-        <div className="w-[50%] h-full menuBg">
+      <div className="w-full homeHeight  flex justify-center ">
+        <div className="w-[50%] h-full menuBg hidden md:block">
           <div className="text-white mt-5 text-[20px] ml-5 font-semibold opacity-75 relative sloganAnimate">{quote[quoteToDisplay]}
           </div>
         </div>
-        <div className="w-[50%] h-full pt-5 text-center">
-          {/* <div className="font-semibold text-[20px] text-white bg-[#A16347] w-[180px] rounded-md flex justify-center m-auto z-10">
-            MENU
-          </div> */}
-          <div>
-          <div className="text-md font-medium text-center mb-10 text-lg">
-          Your lunch is here. Please confirm if you would like to have it.
+        <div className="md:w-[50%] w-full h-full pt-5 text-center">
+          <div className="">
+          <div className="text-md font-medium text-center mb-7 text-lg md:block hidden">
+          Your lunch is here.<div className="block">Please confirm if you would like to have it.</div> 
+          </div>
+          <div className="text-md font-medium text-center mb-10 text-sm block md:hidden">
+          It&apos;s lunchtime!<div className='whitespace-nowrap'> Would you like to have your meal?</div>
           </div>
             <MenuBox menuItems={props.menuData} backgroundColor={""} />
           </div>
-          {/* <div className="text-md font-medium text-center mt-5">
-          Your lunch is here. Please confirm if you would like to have it.
-          </div> */}
-          <div className="flex w-full justify-center items-center gap-10 mt-10 relative">
+          <div className="flex w-full justify-center items-center gap-10 mt-7 relative">
             <button
-              className={`w-20 h-20 font-bold text-[30px] text-green-800 shadow-md bg-white flex items-center justify-center rounded-md relative ${
+              className={`w-[60px] h-[60px] font-bold text-[30px] text-green-800 shadow-md bg-white flex items-center justify-center rounded-md relative ${
                 animate == "yes" ? "buttonAnimation" : ""
               } ${haveLunch == "yes" ? "Outline" : ""} Border`}
               onMouseOver={mouseOverfn}
@@ -117,7 +115,7 @@ export default function Home(props: { menuData: string[],haveLunch:string,id:str
               ✓
             </button>
             <button
-              className={`w-20 h-20 font-bold text-[30px] text-red-800 shadow-md bg-white flex items-center justify-center rounded-md relative ${
+              className={`w-[60px] h-[60px] font-bold text-[30px] text-red-800 shadow-md bg-white flex items-center justify-center rounded-md relative ${
                 animate == "no" ? "buttonAnimation" : ""
               } ${haveLunch == "no" ? "Outline" : ""} Border`}
               onMouseEnter={mouseOverfn}
@@ -128,21 +126,22 @@ export default function Home(props: { menuData: string[],haveLunch:string,id:str
               X
             </button>
           </div>
+          
           <button
-            className="mt-3 p-1  font-semibold bg-black text-white  rounded-md text-md active:bg-zinc-500 z-1"
+            className="mt-5 p-2  font-semibold bg-black text-white  rounded-md text-md active:bg-zinc-500 z-1"
             onClick={submitClick}
           >
-         Submit
+        Submit
           </button>
+         
         </div>
       </div>
     
-      <div className="border-box h-[28px] text-xs flex items-center justify-center text-center fixed bottom-0 right-0 left-0 bg-black text-white">
+      <div className=" border-box h-[28px] text-xs items-center justify-center text-center fixed bottom-0 right-0 left-0 bg-black text-white">
         &copy; 2023 SDETTECH. All rights reserved.
       </div>
       </div>
       {showPopUp &&<div><SubmitPopUp handleClose={()=>{setShowPopUp(!showPopUp)}}/></div>}
-      {showAlert&& <div className="bg-red-300"><PopUp message="Please select one of options" changeView={()=>setShowAlert(!showAlert)}/></div>}
+      {showAlert&& <div><PopUp message="Please select one of options" changeView={()=>setShowAlert(!showAlert)}/></div>}
 </div>
-  );
-}
+  )}
