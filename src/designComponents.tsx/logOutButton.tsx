adb7e129c68b4853430ae0ebe0e6dfaddd19d59f
PolaSanthoshi <polasanthoshi39@gmail.com>
2023-09-15 17:54:01 +0530

    Logout_Issue

--- src/designComponents.tsx/logOutButton.tsx
+++ src/designComponents.tsx/logOutButton.tsx
@@ -3,14 +3,14 @@ import { useRouter } from "next/router";
 
 export default function LogoutButton(props:{class:string}){
     const router=useRouter();
-    function onLogoutClick(){
-        axios.get('/api/logout')
-        .then(response=>router.push('/'))
-       
-    }
+    const  handleLogout = async() => {
+        await axios.get('/.netlify/functions/logout')
+         .then(list=>console.log(list.data))
+         router.push('/')
+       };
     return <div>
        
-         <button className={`font-bold  p-2 rounded-md text-xs md:text-base  ${props.class}`} onClick={()=>onLogoutClick()} >Logout</button>
+         <button className={`font-bold  p-2 rounded-md text-xs md:text-base  ${props.class}`} onClick={handleLogout} >Logout</button>
         
     </div>
 }
--- src/designComponents.tsx/menuBox.tsx
+++ src/designComponents.tsx/menuBox.tsx
@@ -1,4 +1,5 @@
 import { FaArrowRight, FaPlane } from "react-icons/fa";
+import getdate from "../models/Date";
 
 export default function MenuBox(props: { menuItems: string[]; backgroundColor: string }) {
 
@@ -7,7 +8,7 @@ export default function MenuBox(props: { menuItems: string[]; backgroundColor: s
       <div className="absolute w-[100px]  bg-white -left-5 -top-5 rounded-full flex justify-center items-center overflow-hidden ">
         <img src='/images/menuLogo.jpg'/>
       </div>
-      <div className="absolute p-1 text-sm font-semibold -top-3 right-3 rounded-md bg-yellow-400">Today &apos;s Menu</div>
+      <div className="absolute p-1 text-sm font-semibold -top-3 right-3 rounded-md bg-yellow-400">Menu | {getdate()}</div>
       {/* <img src='/images/arrow.jpg '/> */}
       <div className="mt-6 overflow-scroll scrollbar  h-[170px] md:h-[190px] font-semibold  ">
       {props.menuItems&&props.menuItems.map((element,index)=><div key={index} className="flex gap-3 items-center   ml-[110px] break-all ">
